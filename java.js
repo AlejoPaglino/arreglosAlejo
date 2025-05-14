2a7d9df7d8772ad3ad43f771855c3022f1fa1ea9
alumno <alumno@Instituto.local>
2025-05-14 08:22:30 -0300

    14/05/25

---
+++ java.js
@@ -0,0 +1,26 @@
+//PUNTO 1
+let colores = ["azul","amarillo","rojo","naranja","celeste"]
+console.log(colores.length); 
+
+//PUNTO 2
+console.log("primer color", colores[0]);
+console.log("ultimo color", colores[colores.length - 1]);
+//PUNTO 3
+console.log("cantidad de elementos", colores.length)
+//PUNTO 4
+colores.push("verde")
+console.log("agregado al final" ,colores)
+//PUNTO 5
+colores.unshift("negro");
+console.log("agregado al principio", colores)
+//PUNTO 6
+colores.pop("verde");
+console.log("eliminado el del final")
+//PUNTO 7
+colores.shift("negro")
+console.log("eliminado el del principio")
+//PUNTO 8
+
+//PUNTO 9
+
+//PUNTO 10
